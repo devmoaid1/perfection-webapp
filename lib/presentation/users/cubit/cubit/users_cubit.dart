56c36b48a79f0e40626d744aabf3361eb4d0336f
devmoaid1 <moaidmohamed1312@gmail.com>
2024-11-12 01:05:36 +0200

    added users cubit

---
+++ lib/presentation/users/cubit/cubit/users_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../data/repositories/user_repository.dart';
+import '../../../../domain/entities/user_entity.dart';
+
+part 'users_state.dart';
+
+class UsersCubit extends Cubit<UsersState> {
+  final UserRepository _userRepository; // inject UserRepository
+  UsersCubit(this._userRepository) : super(const UsersState());
+
+  void fetchUsers(int page) async {
+    final users =
+        await _userRepository.getUsers(page: page); // get users from repository
+    users.fold(
+        (failure) => emit(state.copyWith(
+            status: Status.error, errorMessage: failure.message)),
+        (users) => emit(state.copyWith(
+            status: Status.success, users: [...state.users, ...users])));
+  }
+}
---
+++ lib/presentation/users/cubit/cubit/users_state.dart
@@ -0,0 +1,38 @@
+part of 'users_cubit.dart';
+
+// Enum to represent different status states
+enum Status { initial, error, loading, success }
+
+class UsersState extends Equatable {
+  final Status status;
+  final List<UserEntity> users;
+  final int currentPage;
+
+  final String? errorMessage;
+
+  const UsersState({
+    this.status = Status.initial,
+    this.users = const [],
+    this.currentPage = 1,
+    this.errorMessage,
+  });
+
+  // copyWith method to create a new state with modified values
+  UsersState copyWith({
+    Status? status,
+    List<UserEntity>? users,
+    int? currentPage,
+    int? totalPages,
+    String? errorMessage,
+  }) {
+    return UsersState(
+      status: status ?? this.status,
+      users: users ?? this.users,
+      currentPage: currentPage ?? this.currentPage,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, users, currentPage, errorMessage];
+}
